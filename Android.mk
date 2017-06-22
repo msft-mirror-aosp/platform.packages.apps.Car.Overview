290d82ad2813d294b222a8d054cc5931b647810c
Bryan Eyler <bryaneyler@google.com>
2017-06-21 17:30:15 -0700

    Disable Car apps in PDK until the dependencies are fixed.
    Bug 62360631
    
    Change-Id: I490c0185b7355a6e24003055655249f9d0e49748

--- Android.mk
+++ Android.mk
@@ -13,6 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+ifneq ($(TARGET_BUILD_PDK), true)
+
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
@@ -70,3 +72,4 @@ include $(CLEAR_VARS)
 
 include $(BUILD_MULTI_PREBUILT)
 
+endif
